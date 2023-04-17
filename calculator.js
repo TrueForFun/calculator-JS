1f074674cc44002b2cdfe2d8f90abe394bfa3b05
Sergei <122358519+TrueForFun@users.noreply.github.com>
2023-04-17 19:37:19 +0000

    update state

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
-let runnungTotal = 0;
-let buffer = `0`;
-let previousOperator = null;
+const initialState = {
+  runningTotal: 0,
+  buffer: "0",
+  previousOperator: null,
+};
 
+let state = { ...initialState };
 const screen = document.querySelector(`.screen`);
 
 function buttonClick(value) {
@@ -12,30 +15,29 @@ function buttonClick(value) {
     handleNumber(value);
     // it is a number
   }
-  screen.innerText = buffer;
+  screen.innerText = state.buffer;
 }
 
 function handleSymbol(symbol) {
   switch (symbol) {
     case `C`:
-      buffer = `0`;
-      runnungTotal = 0;
+      clear();
       break;
     case `=`:
-      if (previousOperator === null) {
+      if (state.previousOperator === null) {
         return;
         // there should be two numbers to math
       }
-      flushOperation(+buffer);
-      previousOperator = null;
-      buffer = runnungTotal;
-      runnungTotal = 0;
+      flushOperation(+state.buffer);
+      state.previousOperator = null;
+      state.buffer = state.runningTotal;
+      state.runningTotal = 0;
       break;
     case `←`:
-      if (buffer.length === 1) {
-        buffer = `0`;
+      if (state.buffer.length === 1) {
+        state.buffer = `0`;
       } else {
-        buffer = buffer.substring(0, buffer.length - 1);
+        state.buffer = state.buffer.substring(0, state.buffer.length - 1);
       }
       break;
     case `+`:
@@ -46,34 +48,39 @@ function handleSymbol(symbol) {
       break;
   }
 }
+
+function clear() {
+  state = initialState;
+}
+
 function handleMath(symbol) {
-  if (buffer === `0`) {
+  if (state.buffer === `0`) {
     return;
   }
 
-  const intBuffer = parseInt(buffer);
+  const intBuffer = parseInt(state.buffer);
   // +buffer will be the same thing
 
-  if (runnungTotal === 0) {
-    runnungTotal = intBuffer;
+  if (state.runningTotal === 0) {
+    state.runningTotal = intBuffer;
   } else {
     flushOperation(intBuffer);
   }
 
-  previousOperator = symbol;
+  state.previousOperator = symbol;
 
-  buffer = `0`;
+  state.buffer = `0`;
 }
 
 function flushOperation(intBuffer) {
   if (previousOperator === `+`) {
-    runnungTotal += intBuffer;
+    runningTotal += intBuffer;
   } else if (previousOperator === `-`) {
-    runnungTotal -= intBuffer;
+    runningTotal -= intBuffer;
   } else if (previousOperator === `×`) {
-    runnungTotal *= intBuffer;
+    runningTotal *= intBuffer;
   } else {
-    runnungTotal /= intBuffer;
+    runningTotal /= intBuffer;
   }
 }
 
